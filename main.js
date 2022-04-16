1c7eba123d0f6205826f8c77a68dbd2cc1694fed
Naozumi <naozumi@naozumi.me>
2022-04-17 01:39:51 +0800

    Didn't know if's unnecessary

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ async function createWindow() {
         devTools: false
       }
     })
+    let overlay;
     initializer.loadURL('https://streamkit.discord.com/overlay')
     const browser = await pie.connect(app, puppeteer)
     const page = await pie.getPage(browser, initializer)
@@ -42,9 +43,9 @@ async function createWindow() {
             message: 'Coudn\'t find the overlay URL.\n\nEnither click both button should be work.\nJust make sure you\'ve clicked the button.'
           })
         }
-        if (overlayUrl) {
+        if (overlayUrl && !overlay) {
           const path = require('path')
-          const overlay = new BrowserWindow({
+          overlay = new BrowserWindow({
             useContentSize: true,
             x: 0,
             y: 0,
@@ -62,10 +63,10 @@ async function createWindow() {
           overlay.setFocusable(false)
           overlay.webContents.setZoomFactor(90)
           overlay.webContents.executeJavaScript(`if (typeof console.oldlog === 'undefined') { console.oldlog = console.log; } window.consoleCatchers = []; console.log = function (text, input) { if (typeof input !== 'undefined') { window.consoleCatchers.forEach(function (item, index) { item(input) }) } else { console.oldlog(text); } }; window.consoleCatchers.push(function (input) { if (input.evt == 'VOICE_STATE_UPDATE') { name = input.data.nick; uState = input.data.voice_state; muteicon = ''; if (uState.self_mute || uState.mute) { muteicon = "<img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAABhMAAAYJQE8CCw1AAAAB3RJTUUH5AUGCx0VMm5EjgAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAABzSURBVDjLxZIxCsAwCEW/oT1P7z93zZJjeIYMv0sCIaBoodTJDz6/JgJfBslOsns1xYONvK66JCeqAC4ALTz+dJvOo0lu/zS87p2C98IdHlq9Buo5D62h17amScMk78hBWXB/DUdP2fyBaINjJiJy4o94AM8J8ksz/MQjAAAAAElFTkSuQmCC' style='height:0.9em;'>"; } deaficon = ''; if (uState.self_deaf || uState.deaf) { deaficon = "<img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAABhMAAAYJQE8CCw1AAAAB3RJTUUH5AUGCx077rhJQQAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAACNSURBVDjLtZPNCcAgDIUboSs4iXTGLuI2XjpBz87g4fWiENr8iNBAQPR9ef7EbfsjAEQAN4A2UtCcGtyMzFxjwVlyBHAwTRFh52gqHDVnF+6L1XJ/w31cp7YvOX/0xlOJ254qYJ1ZLTAmPWeuDVxARDurfBFR8jovMLEKWxG6c1qB55pEuQOpE8vKz30AhEdNuXK0IugAAAAASUVORK5CYII=' style='height:0.9em;'>"; } spans = document.getElementsByTagName('span'); for (i = 0; i < spans.length; i++) { if (spans[i].innerHTML.startsWith(name)) { text = name + muteicon + deaficon; spans[i].innerHTML = text; } } } });`)
-          overlayUrl = overlayUrl.replaceAll('true', 'True')
-          overlay.loadURL(overlayUrl)
+          overlay.loadURL(overlayUrl.replaceAll('true', 'True'))
           await page.waitForSelector('#app-mount > div > div > div.install.is-open > div.config-link > div:nth-child(3) > input', { hidden: true, timeout: 0 })
-          return overlay.close()
+          overlay.close()
+          return overlay = undefined;
         }
       }
     })
