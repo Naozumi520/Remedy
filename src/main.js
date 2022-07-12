6936a6c2e3134f3187dd889e7cc2885e7fba9c99
Naozumi <naozumi@naozumi.me>
2022-07-12 22:04:26 +0800

    Tidy codes

--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
-const { Client } = require('discord.js-selfbot-v13');
-const client = new Client();
-const { app, BrowserWindow, screen, Menu, Tray, nativeTheme, nativeImage } = require('electron')
-const path = require("path")
-const storage = require('electron-json-storage');
+const { Client } = require('discord.js-selfbot-v13')
+const client = new Client()
+const { app, BrowserWindow, Menu, Tray, nativeTheme, nativeImage } = require('electron')
+const path = require('path')
+const storage = require('electron-json-storage')
 const pie = require('puppeteer-in-electron')
 const puppeteer = require('puppeteer-core')
 const { preferences } = require('./preferences.js')
-let tray, page, service, overlay, serverId, channelId;
-let overlayUnpinned = false;
+let tray, page, service, overlay, serverId, channelId
+let overlayUnpinned = false
 
 nativeTheme.themeSource = 'dark'
-
-const trayIcon = nativeImage.createFromPath(path.join(__dirname, '/icon/favicon.png')).resize({
-  width: 18,
-  height: 18,
-});
 
 let windowState = {
   x: 0,
@@ -24,33 +19,32 @@ if (process.platform === 'darwin') {
   app.dock.hide()
 }
 
-function log(message, type) {
-  message.split('\n').forEach(line => console.log(`remedy [${type || 'info'}]: ${line}`))
+function log (message, type) {
+  message.split('\n').forEach(line => console.log(`Remedy Pro [${type || 'info'}]: ${line}`))
 };
 
 client.on('ready', async () => {
-  log(`logged in with account ${client.user.username}.`);
+  log(`logged in with account ${client.user.username}.`)
 })
 
-async function initialize() {
+async function initialize () {
   log(`Starting services\nVersion ${require('../package.json').version}`, 'client')
   await pie.initialize(app)
 }
 
-async function createWindow() {
+async function createWindow () {
   log('Creating window...', 'client')
   storage.get('screenPosition', function (error, object) {
     if (object.windowState) {
       log('Restoring overlay position...', 'client')
-      windowState = object.windowState;
-      return;
+      windowState = object.windowState
     }
-  });
-  tray = new Tray(trayIcon);
-  function Createmenu(tab1, tab2, tab3) {
+  })
+  function Createmenu (tab1, tab2, tab3) {
     const contextMenu = Menu.buildFromTemplate([
       {
-        label: tab1, click: function () {
+        label: tab1,
+        click: function () {
           try {
             preferences.prefsWindow.show()
           } catch (e) {
@@ -59,7 +53,8 @@ async function createWindow() {
         }
       },
       {
-        label: tab2, click: async function () {
+        label: tab2,
+        click: async function () {
           Createmenu('Preferences menu', !overlayUnpinned ? 'Pin overlay' : 'Unpin overlay', 'Close')
           overlayUnpinned = !overlayUnpinned
           if (overlayUnpinned) {
@@ -74,21 +69,21 @@ async function createWindow() {
         }
       },
       {
-        label: tab3, click: function () {
-          log('exited application with code 0')
+        label: tab3,
+        click: function () {
+          log('Exited application with code 0')
           return app.quit()
         }
       }
-    ]);
-    tray.setContextMenu(contextMenu);
+    ])
+    tray.setContextMenu(contextMenu)
   }
   Createmenu('Preferences menu', 'Unpin overlay', 'Close')
-  const size = screen.getPrimaryDisplay().workAreaSize
   service = new BrowserWindow({
     title: 'DiscordOverlayMac',
     icon: './src/assets/icon/favicon.png',
-    width: size.width / 1.75,
-    height: size.height / 1.75,
+    width: 0,
+    height: 0,
     fullscreen: false,
     minimizable: false,
     center: true,
@@ -96,29 +91,34 @@ async function createWindow() {
       backgroundThrottling: false,
       devTools: false
     },
-    show: false,
+    show: false
   })
-  service.loadURL('')
+  service.loadURL('https://streamkit.discord.com/overlay')
   const browser = await pie.connect(app, puppeteer)
   page = await pie.getPage(browser, service)
   await page.goto('https://streamkit.discord.com/overlay')
-  await page.waitForSelector('.install-button button', { timeout: 0 });
+  await page.waitForSelector('.install-button button', { timeout: 0 })
   await page.evaluate(function (selector) {
-    document.querySelector(selector).click();
+    document.querySelector(selector).click()
   }, '.install-button button')
-  await page.waitForSelector('button[value=voice]', { timeout: 0 });
+  await page.waitForSelector('button[value=voice]', { timeout: 0 })
   await page.evaluate(function (selector) {
-    document.querySelector(selector).click();
+    document.querySelector(selector).click()
   }, 'button[value=voice]')
 }
 initialize()
 
 app.on('ready', async () => {
+  const trayIcon = nativeImage.createFromPath(path.join(__dirname, '/icon/favicon.png')).resize({
+    width: 18,
+    height: 18
+  })
+  tray = new Tray(trayIcon)
   createWindow()
 })
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
-  const member = newState.member;
+  const member = newState.member
   if (member.user == client.user) {
     if (oldState.selfMute === true && newState.selfMute === false) {
       return
@@ -133,9 +133,9 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
       return
     }
     if (newState.channelId !== oldState.channelId && !oldState.channelId) {
-      serverId = newState.guild.id;
-      channelId = newState.channelId;
-      log(`User joined voice channel:\nServer ID: ${serverId}\nChannel ID: ${channelId}`, 'voiceStateUpdate');
+      serverId = newState.guild.id
+      channelId = newState.channelId
+      log(`User joined voice channel:\nServer ID: ${serverId}\nChannel ID: ${channelId}`, 'voiceStateUpdate')
       if (!overlay) {
         overlay = new BrowserWindow({
           title: 'Remedy Overlay Component',
@@ -156,8 +156,8 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
         overlay.setIgnoreMouseEvents(true)
         overlay.setFocusable(false)
         overlay.webContents.setZoomFactor(90)
-        overlay.webContents.executeJavaScript(`if (typeof console.oldlog === 'undefined') { console.oldlog = console.log; } window.consoleCatchers = []; console.log = function (text, input) { if (typeof input !== 'undefined') { window.consoleCatchers.forEach(function (item, index) { item(input) }) } else { console.oldlog(text); } }; window.consoleCatchers.push(function (input) { if (input.evt == 'VOICE_STATE_UPDATE') { name = input.data.nick; uState = input.data.voice_state; muteicon = ''; if (uState.self_mute || uState.mute) { muteicon = "<img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAABhMAAAYJQE8CCw1AAAAB3RJTUUH5AUGCx0VMm5EjgAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAABzSURBVDjLxZIxCsAwCEW/oT1P7z93zZJjeIYMv0sCIaBoodTJDz6/JgJfBslOsns1xYONvK66JCeqAC4ALTz+dJvOo0lu/zS87p2C98IdHlq9Buo5D62h17amScMk78hBWXB/DUdP2fyBaINjJiJy4o94AM8J8ksz/MQjAAAAAElFTkSuQmCC' style='height:0.9em;padding-left:5px;transform: translateY(1px);'>"; } deaficon = ''; if (uState.self_deaf || uState.deaf) { deaficon = "<img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAABhMAAAYJQE8CCw1AAAAB3RJTUUH5AUGCx077rhJQQAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAACNSURBVDjLtZPNCcAgDIUboSs4iXTGLuI2XjpBz87g4fWiENr8iNBAQPR9ef7EbfsjAEQAN4A2UtCcGtyMzFxjwVlyBHAwTRFh52gqHDVnF+6L1XJ/w31cp7YvOX/0xlOJ254qYJ1ZLTAmPWeuDVxARDurfBFR8jovMLEKWxG6c1qB55pEuQOpE8vKz30AhEdNuXK0IugAAAAASUVORK5CYII=' style='height:0.9em;padding-left:5px;transform: translateY(1px);'>"; } spans = document.getElementsByTagName('span'); for (i = 0; i < spans.length; i++) { if (spans[i].innerHTML.startsWith(name)) { text = name + muteicon + deaficon; spans[i].innerHTML = text; } } } });`)
-        let url = `https://streamkit.discord.com/overlay/voice/${serverId}/${channelId}?icon=true&online=true&logo=white&text_color=${preferences.preferences.Interface.txt_color.replace('#', '%23')}&text_size=${preferences.preferences.Interface.txt_size}&text_outline_color=${preferences.preferences.Interface.txt_outline_color.replace('#', '%23')}&text_outline_size=${preferences.preferences.Interface.txt_outline_size}&text_shadow_color=${preferences.preferences.Interface.txt_shadow_color.replace('#', '%23')}&text_shadow_size=${preferences.preferences.Interface.txt_shadow_size}&bg_color=${preferences.preferences.Interface.bg_color.replace('#', '%23')}&bg_opacity=${parseFloat(preferences.preferences.Interface.bg_opacity) / 100}&bg_shadow_color=${preferences.preferences.Interface.bg_shadow_color.replace('#', '%23')}&bg_shadow_size=${preferences.preferences.Interface.bg_shadow_size}&invite_code=&limit_speaking=${preferences.preferences.Interface.general.includes('users_only')}&small_avatars=${preferences.preferences.Interface.general.includes('small_avt')}&hide_names=${preferences.preferences.Interface.general.includes('hide_nick')}&fade_chat=0`
+        overlay.webContents.executeJavaScript('if (typeof console.oldlog === \'undefined\') { console.oldlog = console.log; } window.consoleCatchers = []; console.log = function (text, input) { if (typeof input !== \'undefined\') { window.consoleCatchers.forEach(function (item, index) { item(input) }) } else { console.oldlog(text); } }; window.consoleCatchers.push(function (input) { if (input.evt == \'VOICE_STATE_UPDATE\') { name = input.data.nick; uState = input.data.voice_state; muteicon = \'\'; if (uState.self_mute || uState.mute) { muteicon = "<img src=\'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAABhMAAAYJQE8CCw1AAAAB3RJTUUH5AUGCx0VMm5EjgAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAABzSURBVDjLxZIxCsAwCEW/oT1P7z93zZJjeIYMv0sCIaBoodTJDz6/JgJfBslOsns1xYONvK66JCeqAC4ALTz+dJvOo0lu/zS87p2C98IdHlq9Buo5D62h17amScMk78hBWXB/DUdP2fyBaINjJiJy4o94AM8J8ksz/MQjAAAAAElFTkSuQmCC\' style=\'height:0.9em;padding-left:3px;transform: translateY(1px);\'>"; } deaficon = \'\'; if (uState.self_deaf || uState.deaf) { deaficon = "<img src=\'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAABhMAAAYJQE8CCw1AAAAB3RJTUUH5AUGCx077rhJQQAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAACNSURBVDjLtZPNCcAgDIUboSs4iXTGLuI2XjpBz87g4fWiENr8iNBAQPR9ef7EbfsjAEQAN4A2UtCcGtyMzFxjwVlyBHAwTRFh52gqHDVnF+6L1XJ/w31cp7YvOX/0xlOJ254qYJ1ZLTAmPWeuDVxARDurfBFR8jovMLEKWxG6c1qB55pEuQOpE8vKz30AhEdNuXK0IugAAAAASUVORK5CYII=\' style=\'height:0.9em;padding-left:3px;transform: translateY(1px);\'>"; } spans = document.getElementsByTagName(\'span\'); for (i = 0; i < spans.length; i++) { if (spans[i].innerHTML.startsWith(name)) { text = name + muteicon + deaficon; spans[i].innerHTML = text; } } } });')
+        const url = `https://streamkit.discord.com/overlay/voice/${serverId}/${channelId}?icon=true&online=true&logo=white&text_color=${preferences.preferences.Interface.txt_color.replace('#', '%23')}&text_size=${preferences.preferences.Interface.txt_size}&text_outline_color=${preferences.preferences.Interface.txt_outline_color.replace('#', '%23')}&text_outline_size=${preferences.preferences.Interface.txt_outline_size}&text_shadow_color=${preferences.preferences.Interface.txt_shadow_color.replace('#', '%23')}&text_shadow_size=${preferences.preferences.Interface.txt_shadow_size}&bg_color=${preferences.preferences.Interface.bg_color.replace('#', '%23')}&bg_opacity=${parseFloat(preferences.preferences.Interface.bg_opacity) / 100}&bg_shadow_color=${preferences.preferences.Interface.bg_shadow_color.replace('#', '%23')}&bg_shadow_size=${preferences.preferences.Interface.bg_shadow_size}&invite_code=&limit_speaking=${preferences.preferences.Interface.general.includes('users_only')}&small_avatars=${preferences.preferences.Interface.general.includes('small_avt')}&hide_names=${preferences.preferences.Interface.general.includes('hide_nick')}&fade_chat=0`
         overlay.loadURL(url.replaceAll('true', 'True'))
         await overlay.webContents.insertCSS(`
           * {
@@ -169,20 +169,20 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
       }
     } else {
       overlay?.close()
-      return overlay = undefined;
+      return overlay = undefined
     }
   }
-});
+})
 
 preferences.on('save', () => {
-  let url = `https://streamkit.discord.com/overlay/voice/${serverId}/${channelId}?icon=true&online=true&logo=white&text_color=${preferences.preferences.Interface.txt_color.replace('#', '%23')}&text_size=${preferences.preferences.Interface.txt_size}&text_outline_color=${preferences.preferences.Interface.txt_outline_color.replace('#', '%23')}&text_outline_size=${preferences.preferences.Interface.txt_outline_size}&text_shadow_color=${preferences.preferences.Interface.txt_shadow_color.replace('#', '%23')}&text_shadow_size=${preferences.preferences.Interface.txt_shadow_size}&bg_color=${preferences.preferences.Interface.bg_color.replace('#', '%23')}&bg_opacity=${parseFloat(preferences.preferences.Interface.bg_opacity) / 100}&bg_shadow_color=${preferences.preferences.Interface.bg_shadow_color.replace('#', '%23')}&bg_shadow_size=${preferences.preferences.Interface.bg_shadow_size}&invite_code=&limit_speaking=${preferences.preferences.Interface.general.includes('users_only')}&small_avatars=${preferences.preferences.Interface.general.includes('small_avt')}&hide_names=${preferences.preferences.Interface.general.includes('hide_nick')}&fade_chat=0`.replaceAll('true', 'True')
+  const url = `https://streamkit.discord.com/overlay/voice/${serverId}/${channelId}?icon=true&online=true&logo=white&text_color=${preferences.preferences.Interface.txt_color.replace('#', '%23')}&text_size=${preferences.preferences.Interface.txt_size}&text_outline_color=${preferences.preferences.Interface.txt_outline_color.replace('#', '%23')}&text_outline_size=${preferences.preferences.Interface.txt_outline_size}&text_shadow_color=${preferences.preferences.Interface.txt_shadow_color.replace('#', '%23')}&text_shadow_size=${preferences.preferences.Interface.txt_shadow_size}&bg_color=${preferences.preferences.Interface.bg_color.replace('#', '%23')}&bg_opacity=${parseFloat(preferences.preferences.Interface.bg_opacity) / 100}&bg_shadow_color=${preferences.preferences.Interface.bg_shadow_color.replace('#', '%23')}&bg_shadow_size=${preferences.preferences.Interface.bg_shadow_size}&invite_code=&limit_speaking=${preferences.preferences.Interface.general.includes('users_only')}&small_avatars=${preferences.preferences.Interface.general.includes('small_avt')}&hide_names=${preferences.preferences.Interface.general.includes('hide_nick')}&fade_chat=0`.replaceAll('true', 'True')
   overlay.loadURL(url)
-});
+})
 
 storage.get('discordToken', function (error, object) {
-  if (error) throw error;
+  if (error) throw error
   if (object.token) {
-    client.login(object.token);
+    client.login(object.token)
   } else {
     const prompt = require('custom-electron-prompt')
     prompt({
@@ -193,12 +193,12 @@ storage.get('discordToken', function (error, object) {
     })
       .then((r) => {
         if (r === null) {
-          log('user cancelled', 'client');
+          log('user cancelled', 'client')
         } else {
           storage.set('discordToken', { token: r })
-          client.login(r);
+          client.login(r)
         }
       })
-      .catch(console.error);
+      .catch(console.error)
   }
-});
+})
--- src/preferences.js
+++ src/preferences.js
@@ -1,183 +1,183 @@
-const { app } = require('electron'),
-    path = require('path'),
-    ElectronPreferences = require('electron-preferences'),
-    appPath = app.getAppPath()
+const { app } = require('electron')
+const path = require('path')
+const ElectronPreferences = require('electron-preferences')
+const appPath = app.getAppPath()
 
 const defaultSettings = {
-    Interface: {
-        general: [],
-        txt_color: '#ffffff',
-        txt_outline_color: '#000000',
-        txt_shadow_color: '#000000',
-        txt_size: '14',
-        txt_outline_size: '0',
-        txt_shadow_size: '0',
-        bg_color: '#1e2124',
-        bg_shadow_color: '#000000',
-        bg_shadow_size: '0',
-        bg_opacity: '95'
-    }
+  Interface: {
+    general: [],
+    txt_color: '#ffffff',
+    txt_outline_color: '#000000',
+    txt_shadow_color: '#000000',
+    txt_size: '14',
+    txt_outline_size: '0',
+    txt_shadow_size: '0',
+    bg_color: '#1e2124',
+    bg_shadow_color: '#000000',
+    bg_shadow_size: '0',
+    bg_opacity: '95'
+  }
 }
 
 const preferences = new ElectronPreferences({
-    dataStore: path.resolve(app.getPath('userData'), 'config.json'),
-    debug: true,
-    defaults: defaultSettings,
-    browserWindowOverrides: {
-        title: `Preferences | ${app.name}`,
-        icon: path.join(appPath, '/src/icon/favicon.png'),
-        resizable: false,
-        maximizable: false,
-    },
-    sections: [
-        {
-            id: 'Interface',
-            label: 'Interface',
-            icon: 'eye-19',
-            form: {
-                groups: [
-                    {
-                        fields: [
-                            {
-                                label: 'GENERAL',
-                                key: 'general',
-                                type: 'checkbox',
-                                options: [
-                                    {
-                                        label: 'Show Speaking Users Only',
-                                        value: 'users_only',
-                                    },
-                                    {
-                                        label: 'Small Avatars',
-                                        value: 'small_avt',
-                                    },
-                                    {
-                                        label: 'Hide Names',
-                                        value: 'hide_nick',
-                                    }
-                                ]
-                            },
-                            {
-                                label: 'TEXT COLOR',
-                                key: 'txt_color',
-                                type: 'color',
-                                format: 'hex',
-                                options: [
-                                    { label: 'Text Color' }
-                                ]
-                            },
-                            {
-                                label: 'TEXT OUTLINE COLOR',
-                                key: 'txt_outline_color',
-                                type: 'color',
-                                format: 'hex',
-                                options: [
-                                    { label: 'Text Outline Color' }
-                                ]
-                            },
-                            {
-                                label: 'TEXT SHADOW COLOR',
-                                key: 'txt_shadow_color',
-                                type: 'color',
-                                format: 'hex',
-                                options: [
-                                    { label: 'Text Shadow Color' }
-                                ]
-                            },
-                            {
-                                label: 'TEXT Size',
-                                key: 'txt_size',
-                                type: 'text',
-                                format: 'hex',
-                                options: [
-                                    { label: 'Text Size' }
-                                ]
-                            },
-                            {
-                                label: 'TEXT OUTLINE Size',
-                                key: 'txt_outline_size',
-                                type: 'text',
-                                format: 'hex',
-                                options: [
-                                    { label: 'Text Outline Size' }
-                                ]
-                            },
-                            {
-                                label: 'TEXT SHADOW Size',
-                                key: 'txt_shadow_size',
-                                type: 'text',
-                                format: 'hex',
-                                options: [
-                                    { label: 'Text Shadow Size' }
-                                ]
-                            },
-                            {
-                                label: 'BACKGROUND COLOR',
-                                key: 'bg_color',
-                                type: 'color',
-                                format: 'hex',
-                                options: [
-                                    { label: 'Background Color' }
-                                ]
-                            },
-                            {
-                                label: 'BACKGROUND SHADOW COLOR',
-                                key: 'bg_shadow_color',
-                                type: 'color',
-                                format: 'hex',
-                                options: [
-                                    { label: 'Background Shadow Color' }
-                                ]
-                            },
-                            {
-                                label: 'BACKGROUND SHADOW SIZE',
-                                key: 'bg_shadow_size',
-                                type: 'text',
-                                format: 'hex',
-                                options: [
-                                    { label: 'Background Shadow Size' }
-                                ]
-                            },
-                            {
-                                label: 'Reset',
-                                key: 'reset_pref',
-                                type: 'button',
-                                buttonLabel: 'Reset preferences',
-                                help: 'Resetting the preferences to default.',
-                                hideLabel: false
-                            },
-                            {
-                                key: 'reset_whole_app',
-                                type: 'button',
-                                buttonLabel: 'Reset! The! Whole! App!',
-                                help: 'Resetting the entire application will roll the program back to its default state.'
-                            }
-                        ]
-                    }
+  dataStore: path.resolve(app.getPath('userData'), 'config.json'),
+  debug: true,
+  defaults: defaultSettings,
+  browserWindowOverrides: {
+    title: `Preferences | ${app.name}`,
+    icon: path.join(appPath, '/src/icon/favicon.png'),
+    resizable: false,
+    maximizable: false
+  },
+  sections: [
+    {
+      id: 'Interface',
+      label: 'Interface',
+      icon: 'eye-19',
+      form: {
+        groups: [
+          {
+            fields: [
+              {
+                label: 'GENERAL',
+                key: 'general',
+                type: 'checkbox',
+                options: [
+                  {
+                    label: 'Show Speaking Users Only',
+                    value: 'users_only'
+                  },
+                  {
+                    label: 'Small Avatars',
+                    value: 'small_avt'
+                  },
+                  {
+                    label: 'Hide Names',
+                    value: 'hide_nick'
+                  }
                 ]
-            }
-        }
-    ]
-});
-
-preferences.on('click', key => {
-    if (key === 'reset_pref') {
-        const defaults = defaultSettings
-        for (const [key, value] of Object.entries(defaults)) {
-            preferences.value(key, value)
-        }
-        setTimeout(() => {
-            app.relaunch();
-            app.exit();
-        }, 5000);
+              },
+              {
+                label: 'TEXT COLOR',
+                key: 'txt_color',
+                type: 'color',
+                format: 'hex',
+                options: [
+                  { label: 'Text Color' }
+                ]
+              },
+              {
+                label: 'TEXT OUTLINE COLOR',
+                key: 'txt_outline_color',
+                type: 'color',
+                format: 'hex',
+                options: [
+                  { label: 'Text Outline Color' }
+                ]
+              },
+              {
+                label: 'TEXT SHADOW COLOR',
+                key: 'txt_shadow_color',
+                type: 'color',
+                format: 'hex',
+                options: [
+                  { label: 'Text Shadow Color' }
+                ]
+              },
+              {
+                label: 'TEXT Size',
+                key: 'txt_size',
+                type: 'text',
+                format: 'hex',
+                options: [
+                  { label: 'Text Size' }
+                ]
+              },
+              {
+                label: 'TEXT OUTLINE Size',
+                key: 'txt_outline_size',
+                type: 'text',
+                format: 'hex',
+                options: [
+                  { label: 'Text Outline Size' }
+                ]
+              },
+              {
+                label: 'TEXT SHADOW Size',
+                key: 'txt_shadow_size',
+                type: 'text',
+                format: 'hex',
+                options: [
+                  { label: 'Text Shadow Size' }
+                ]
+              },
+              {
+                label: 'BACKGROUND COLOR',
+                key: 'bg_color',
+                type: 'color',
+                format: 'hex',
+                options: [
+                  { label: 'Background Color' }
+                ]
+              },
+              {
+                label: 'BACKGROUND SHADOW COLOR',
+                key: 'bg_shadow_color',
+                type: 'color',
+                format: 'hex',
+                options: [
+                  { label: 'Background Shadow Color' }
+                ]
+              },
+              {
+                label: 'BACKGROUND SHADOW SIZE',
+                key: 'bg_shadow_size',
+                type: 'text',
+                format: 'hex',
+                options: [
+                  { label: 'Background Shadow Size' }
+                ]
+              },
+              {
+                label: 'Reset',
+                key: 'reset_pref',
+                type: 'button',
+                buttonLabel: 'Reset preferences',
+                help: 'Resetting the preferences to default.',
+                hideLabel: false
+              },
+              {
+                key: 'reset_whole_app',
+                type: 'button',
+                buttonLabel: 'Reset! The! Whole! App!',
+                help: 'Resetting the entire application will roll the program back to its default state.'
+              }
+            ]
+          }
+        ]
+      }
     }
-    if (key === 'reset_whole_app') {
-        const fs = require('fs');
-        fs.rm(app.getPath('userData'), { recursive: true, force: true }, () => {
-            app.relaunch();
-            app.exit();
-        });
-    }
+  ]
 })
 
-module.exports = { preferences };
+preferences.on('click', key => {
+  if (key === 'reset_pref') {
+    const defaults = defaultSettings
+    for (const [key, value] of Object.entries(defaults)) {
+      preferences.value(key, value)
+    }
+    setTimeout(() => {
+      app.relaunch()
+      app.exit()
+    }, 5000)
+  }
+  if (key === 'reset_whole_app') {
+    const fs = require('fs')
+    fs.rm(app.getPath('userData'), { recursive: true, force: true }, () => {
+      app.relaunch()
+      app.exit()
+    })
+  }
+})
+
+module.exports = { preferences }
